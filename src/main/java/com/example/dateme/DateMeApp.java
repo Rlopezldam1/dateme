fb34bd6fd720895190a1ddf0c7810a421ce297d2
Rlopezldam1 <rlopezldam1@gmail.com>
2023-05-17 13:31:55 +0200

    Creado controlador main. Añadidos los id de los objetos de la página main.

--- src/main/java/com/example/dateme/DateMeApp.java
+++ src/main/java/com/example/dateme/DateMeApp.java
@@ -23,15 +23,21 @@ public class DateMeApp extends Application {
         FXMLLoader homeloader = new FXMLLoader(this.getClass().getResource("home/home.fxml"));
         FXMLLoader mensajesloader = new FXMLLoader(this.getClass().getResource("mensajes/mensajes.fxml"));
         FXMLLoader historialloader = new FXMLLoader(this.getClass().getResource("historial/historial.fxml"));
+        FXMLLoader mainloader = new FXMLLoader(this.getClass().getResource("mainpage/mainpage.fxml"));
         Scene login = new Scene(loginloader.load(), 480, 630);
         Scene signup = new Scene(signuploader.load(), 480, 630);
         Scene home = new Scene(homeloader.load(), 900, 600);
         Scene mensajes = new Scene(mensajesloader.load(), 600, 400);
         Scene historial = new Scene(historialloader.load(), 600, 400);
+        Scene main = new Scene(mainloader.load(), 900, 600);
         dateMe.setTitle("DateMe");
-        dateMe.setScene(mensajes);
+        dateMe.setScene(main);
         dateMe.setResizable(false);
         dateMe.show();
+    }
+
+    public static void cambiarPestana(String ruta) {
+
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/dateme/MainController.java
@@ -0,0 +1,54 @@
+package com.example.dateme;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+
+public class MainController {
+
+    @FXML
+    private Button ajustes;
+
+    @FXML
+    private ImageView botonDislike;
+
+    @FXML
+    private Button botonLike;
+
+    @FXML
+    private Button chats;
+
+    @FXML
+    private Pane fotoPerfil;
+
+    @FXML
+    private Button matches;
+
+    @FXML
+    void ajustesClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void chatsClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void dislikeClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void likeClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void matchesClick(MouseEvent event) {
+
+    }
+
+}
