55b2cddfc96d8869622d49d63feefe9f4e30221b
Rlopezldam1 <rlopezldam1@gmail.com>
2023-05-18 13:13:19 +0200

    Modificada página settings y mejorada página mensajes

---
+++ src/main/java/com/example/dateme/AjustesController.java
@@ -0,0 +1,30 @@
+package com.example.dateme;
+
+import javafx.event.ActionEvent;
+
+public class AjustesController {
+    public void editarPerfilClick(ActionEvent actionEvent) {
+    }
+
+    public void cambiarPreferenciasClick(ActionEvent actionEvent) {
+    }
+
+    public void configuracionPrivacidadClick(ActionEvent actionEvent) {
+    }
+
+    public void cerrarSesionClick(ActionEvent actionEvent) {
+
+    }
+
+    public void ajustesClick(ActionEvent actionEvent) {
+    }
+
+    public void chatsClick(ActionEvent actionEvent) {
+    }
+
+    public void matchesClick(ActionEvent actionEvent) {
+    }
+
+    public void inicioClick(ActionEvent actionEvent) {
+    }
+}
