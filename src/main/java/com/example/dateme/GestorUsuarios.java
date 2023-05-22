363eb9a0ab92c8762a6be1656bebacec6011a91e
jtrigosdam1 <jtrigosdam1@gmail.com>
2023-05-22 10:02:41 +0200

    Borrados metodos que no se usan en GestorUsuarios

--- src/main/java/com/example/dateme/GestorUsuarios.java
+++ src/main/java/com/example/dateme/GestorUsuarios.java
@@ -134,16 +134,6 @@ public class GestorUsuarios {
         return GestionDatosBBDD.consultarUsuarioVisitadoBBDD(usuarioActual, usuarioVisitado);
     }
 
-    public ArrayList<Usuario> generoCompatible(Usuario.Genero genero) {
-        ArrayList<Usuario> usuariosCompatibles = new ArrayList<>();
-        for (Usuario usuario : usuarios) {
-            if (usuario.getGeneroUsuario().equals(genero)) {
-                usuariosCompatibles.add(usuario);
-            }
-        }
-        return usuariosCompatibles;
-    }
-
     public static Usuario existeUsuario(String id) {
         for (Usuario usuario : usuarios) {
             if (usuario.getIdUsuario().equals(id)) {
@@ -151,47 +141,5 @@ public class GestorUsuarios {
             }
         }
         return null;
-    }
-
-    public ArrayList<Usuario> edadCompatible(ArrayList<ArrayList<Integer>> arraysEdades) {
-        ArrayList<Integer> todasLasEdades = new ArrayList<>();
-        for (ArrayList<Integer> arrayEdades : arraysEdades) {
-            todasLasEdades.addAll(arrayEdades);
-        }
-        ArrayList<Usuario> usuariosCompatibles = new ArrayList<>();
-        for (Usuario usuario : usuarios) {
-            if (todasLasEdades.contains(usuario.getEdadUsuario())) {
-                usuariosCompatibles.add(usuario);
-            }
-        }
-        return usuariosCompatibles;
-    }
-
-    public ArrayList<Usuario> localidadCompatible(String localidad) {
-        ArrayList<Usuario> usuariosCompatibles = new ArrayList<>();
-        for (Usuario usuario : usuarios) {
-            if (usuario.getLocalidadUsuario().equals(localidad));
-            usuariosCompatibles.add(usuario);
-        }
-        return usuariosCompatibles;
-    }
-
-    public static void main(String[] args) {
-        String id = "josebatrigo";
-        String nom = "Joseba";
-        String ap = "Trigo";
-        String co = "root";
-        String loc = "Navarra";
-        String corr = "jtrigosier1@educacion.navarra.es";
-        LocalDate fechaNacimiento = LocalDate.of(2004, 1, 8);
-        String des = "Me gustan las tetas grandes";
-        Usuario.Genero gen = Usuario.Genero.HOMBRE;
-        Image im = null;
-        ArrayList<Usuario.Genero> preG = new ArrayList<>();
-        preG.add(Usuario.Genero.MUJER);
-        ArrayList<Integer> preE = new ArrayList<>();
-        preE.add(18);
-        Usuario usuario = new Usuario(id, nom, ap, co, loc, corr, fechaNacimiento, des, gen, im, preG, preE);
-        GestorUsuarios.addUsuario(usuario);
     }
 }
